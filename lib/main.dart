3d9cb1a9aebec59e41f268225fb512b15dcb1504
Varrel <54306896+Varrel-18@users.noreply.github.com>
2020-07-20 19:40:59 +0700

    Login Function

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,28 @@ import 'package:sampleProject2/splash.dart';
 import 'sign_in.dart';
 import 'Home.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: LoginPage(),
+//       initialRoute: '/login',
+//       routes: <String, WidgetBuilder>{
+//         '/splash': (context) => Splash(),
+//         '/login': (context) => LoginPage(),
+//         '/Home': (context) => HomeState(),
+//       },
+//     );
+//   }
+// }
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -11,12 +32,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-      initialRoute: '/login',
-      routes: <String, WidgetBuilder>{
-        '/splash': (context) => Splash(),
-        '/login': (context) => LoginPage(),
-        '/Home': (context) => HomeState(),
+      title: 'SRM',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      routes: {
+        '/': (_) => Splash(),
+        '/login': (_) => LoginPage(),
+        '/home': (_) => HomeState(),
       },
     );
   }
--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'Home.dart';
-// import "package:googleapis_auth/auth_browser.dart";
-// import 'package:googleapis/storage/v1.dart';
-// import 'package:googleapis_auth/auth_io.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-import 'dart:io' show Platform;
-import 'package:http/http.dart' as http;
-import 'package:flutter_appauth/flutter_appauth.dart';
 
 // final authorizationEndpoint =
 //     Uri.parse("http://example.com/oauth2/authorization");
@@ -52,262 +46,19 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  bool _isBusy = false;
-  final FlutterAppAuth _appAuth = FlutterAppAuth();
-  String _codeVerifier;
-  String _authorizationCode;
-  String _refreshToken;
-  String _accessToken;
-  final TextEditingController _authorizationCodeTextController =
-      TextEditingController();
-  final TextEditingController _accessTokenTextController =
-      TextEditingController();
-  final TextEditingController _accessTokenExpirationTextController =
-      TextEditingController();
-
-  final TextEditingController _idTokenTextController = TextEditingController();
-  final TextEditingController _refreshTokenTextController =
-      TextEditingController();
-  String _userInfo = '';
-
-  // For a list of client IDs, go to https://demo.identityserver.io
-  final String _clientId = '';
-  final String _redirectUrl = '';
-  final String _issuer = '';
-  final String _discoveryUrl = '';
-  final List<String> _scopes = <String>[
-    'openid',
-    'profile',
-    'email',
-    'offline_access',
-    'api'
-  ];
-
-  final AuthorizationServiceConfiguration _serviceConfiguration =
-      AuthorizationServiceConfiguration(
-          'https://demo.identityserver.io/connect/authorize',
-          'https://demo.identityserver.io/connect/token');
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Visibility(
-                visible: _isBusy,
-                child: const LinearProgressIndicator(),
-              ),
-              RaisedButton(
-                child: const Text('Sign in with no code exchange'),
-                onPressed: _signInWithNoCodeExchange,
-              ),
-              RaisedButton(
-                child: const Text('Exchange code'),
-                onPressed: _authorizationCode != null ? _exchangeCode : null,
-              ),
-              RaisedButton(
-                child: const Text('Sign in with auto code exchange'),
-                onPressed: () => _signInWithAutoCodeExchange(),
-              ),
-              if (Platform.isIOS)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    child: const Text(
-                      'Sign in with auto code exchange using ephemeral session (iOS only)',
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: () => _signInWithAutoCodeExchange(
-                        preferEphemeralSession: true),
-                  ),
-                ),
-              RaisedButton(
-                child: const Text('Refresh token'),
-                onPressed: _refreshToken != null ? _refresh : null,
-              ),
-              const Text('authorization code'),
-              TextField(
-                controller: _authorizationCodeTextController,
-              ),
-              const Text('access token'),
-              TextField(
-                controller: _accessTokenTextController,
-              ),
-              const Text('access token expiration'),
-              TextField(
-                controller: _accessTokenExpirationTextController,
-              ),
-              const Text('id token'),
-              TextField(
-                controller: _idTokenTextController,
-              ),
-              const Text('refresh token'),
-              TextField(
-                controller: _refreshTokenTextController,
-              ),
-              const Text('test api results'),
-              Text(_userInfo),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> _refresh() async {
-    try {
-      _setBusyState();
-      final TokenResponse result = await _appAuth.token(TokenRequest(
-          _clientId, _redirectUrl,
-          refreshToken: _refreshToken,
-          discoveryUrl: _discoveryUrl,
-          scopes: _scopes));
-      _processTokenResponse(result);
-      await _testApi(result);
-    } catch (_) {
-      _clearBusyState();
-    }
-  }
-
-  Future<void> _exchangeCode() async {
-    try {
-      _setBusyState();
-      final TokenResponse result = await _appAuth.token(TokenRequest(
-          _clientId, _redirectUrl,
-          authorizationCode: _authorizationCode,
-          discoveryUrl: _discoveryUrl,
-          codeVerifier: _codeVerifier,
-          scopes: _scopes));
-      _processTokenResponse(result);
-      await _testApi(result);
-    } catch (_) {
-      _clearBusyState();
-    }
-  }
-
-  Future<void> _signInWithNoCodeExchange() async {
-    try {
-      _setBusyState();
-      // use the discovery endpoint to find the configuration
-      final AuthorizationResponse result = await _appAuth.authorize(
-        AuthorizationRequest(_clientId, _redirectUrl,
-            discoveryUrl: _discoveryUrl, scopes: _scopes, loginHint: 'bob'),
-      );
-
-      // or just use the issuer
-      // var result = await _appAuth.authorize(
-      //   AuthorizationRequest(
-      //     _clientId,
-      //     _redirectUrl,
-      //     issuer: _issuer,
-      //     scopes: _scopes,
-      //   ),
-      // );
-      if (result != null) {
-        _processAuthResponse(result);
-      }
-    } catch (_) {
-      _clearBusyState();
-    }
-  }
-
-  Future<void> _signInWithAutoCodeExchange(
-      {bool preferEphemeralSession = false}) async {
-    try {
-      _setBusyState();
-
-      // show that we can also explicitly specify the endpoints rather than getting from the details from the discovery document
-      final AuthorizationTokenResponse result =
-          await _appAuth.authorizeAndExchangeCode(
-        AuthorizationTokenRequest(
-          _clientId,
-          _redirectUrl,
-          serviceConfiguration: _serviceConfiguration,
-          scopes: _scopes,
-          preferEphemeralSession: preferEphemeralSession,
-        ),
-      );
-
-      // this code block demonstrates passing in values for the prompt parameter. in this case it prompts the user login even if they have already signed in. the list of supported values depends on the identity provider
-      // final AuthorizationTokenResponse result = await _appAuth.authorizeAndExchangeCode(
-      //   AuthorizationTokenRequest(_clientId, _redirectUrl,
-      //       serviceConfiguration: _serviceConfiguration,
-      //       scopes: _scopes,
-      //       promptValues: ['login']),
-      // );
-
-      if (result != null) {
-        _processAuthTokenResponse(result);
-        await _testApi(result);
-      }
-    } catch (_) {
-      _clearBusyState();
-    }
-  }
-
-  void _clearBusyState() {
-    setState(() {
-      _isBusy = false;
-    });
-  }
-
-  void _setBusyState() {
-    setState(() {
-      _isBusy = true;
-    });
-  }
-
-  void _processAuthTokenResponse(AuthorizationTokenResponse response) {
-    setState(() {
-      _accessToken = _accessTokenTextController.text = response.accessToken;
-      _idTokenTextController.text = response.idToken;
-      _refreshToken = _refreshTokenTextController.text = response.refreshToken;
-      _accessTokenExpirationTextController.text =
-          response.accessTokenExpirationDateTime?.toIso8601String();
-    });
-  }
-
-  void _processAuthResponse(AuthorizationResponse response) {
-    setState(() {
-      // save the code verifier as it must be used when exchanging the token
-      _codeVerifier = response.codeVerifier;
-      _authorizationCode =
-          _authorizationCodeTextController.text = response.authorizationCode;
-      _isBusy = false;
-    });
-  }
-
-  void _processTokenResponse(TokenResponse response) {
-    setState(() {
-      _accessToken = _accessTokenTextController.text = response.accessToken;
-      _idTokenTextController.text = response.idToken;
-      _refreshToken = _refreshTokenTextController.text = response.refreshToken;
-      _accessTokenExpirationTextController.text =
-          response.accessTokenExpirationDateTime?.toIso8601String();
-    });
-  }
-
-  Future<void> _testApi(TokenResponse response) async {
-    final http.Response httpResponse = await http.get(
-        'https://demo.identityserver.io/api/test',
-        headers: <String, String>{'Authorization': 'Bearer $_accessToken'});
-    setState(() {
-      _userInfo = httpResponse.statusCode == 200 ? httpResponse.body : '';
-      _isBusy = false;
-    });
-  }
+class Strings {
+  static const String clientId =
+      "445931920297-a0nldcfbu1hbqne9dlov3ga9ih0b238u.apps.googleusercontent.com";
 }
+
+class _LoginPageState extends State<LoginPage> {
+  GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ],
+      clientId:
+          "445931920297-1nmg9s1u4u7koblf7si78ub82mu4pruf.apps.googleusercontent.com");
 //===============================================================================================
 // Future<void> doLogin() async {
 //   var hlp = OAuth2Helper(GoogleOAuth2Client(
@@ -479,109 +230,122 @@ class _LoginPageState extends State<LoginPage> {
 // }
 //============================================================================
 
-// @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     body: Container(
-//       color: Colors.blue,
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.only(left: 60),
-//             child: Text(
-//               'Sign In',
-//               style: TextStyle(
-//                   color: Colors.white, fontFamily: 'Raleway', fontSize: 40),
-//             ),
-//           ),
-//           Padding(
-//             padding:
-//                 const EdgeInsets.only(top: 20, right: 60, left: 60, bottom: 0),
-//             child: Text(
-//               'Username:',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontFamily: 'Raleway',
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: EdgeInsets.only(right: 60, left: 60),
-//             child: Container(
-//               child: TextFormField(),
-//             ),
-//           ),
-//           Padding(
-//             padding:
-//                 const EdgeInsets.only(top: 20, right: 60, left: 60, bottom: 0),
-//             child: Text(
-//               'Password:',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontFamily: 'Raleway',
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: EdgeInsets.only(right: 60, left: 60),
-//             child: Container(
-//               child: TextFormField(),
-//             ),
-//           ),
-//           Column(
-//             children: [
-//               Padding(
-//                 padding: EdgeInsets.only(top: 14, bottom: 2),
-//                 child: ButtonTheme(
-//                   minWidth: 200.0,
-//                   child: RaisedButton(
-//                     shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(18.0),
-//                         side: BorderSide(color: Colors.white)),
-//                     onPressed: () {
-//                       Navigator.push(
-//                         context,
-//                         MaterialPageRoute(builder: (context) => HomeState()),
-//                       );
-//                     },
-//                     color: Colors.blue,
-//                     textColor: Colors.white,
-//                     child: Text("Submit".toUpperCase(),
-//                         style: TextStyle(fontSize: 14)),
-//                   ),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: EdgeInsets.only(top: 0, right: 80, left: 80),
-//                 child: ButtonTheme(
-//                   minWidth: 200.0,
-//                   child: RaisedButton(
-//                     shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(22.0),
-//                         side: BorderSide(color: Colors.white)),
-//                     onPressed: () {},
-//                     child: ListTile(
-//                       leading:
-//                           Image.asset("assets/Image/google.png", width: 30),
-//                       title: Text(
-//                         "Sign in with google".toUpperCase(),
-//                         style: TextStyle(fontSize: 12),
-//                       ),
-//                     ),
-//                     color: Colors.white,
-//                     textColor: Colors.black,
-//                   ),
-//                 ),
-//               )
-//             ],
-//           ),
-//         ],
-//       ),
-//     ),
-//   );
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 60),
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                    color: Colors.white, fontFamily: 'Raleway', fontSize: 40),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20, right: 60, left: 60, bottom: 0),
+              child: Text(
+                'Username:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Raleway',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 60, left: 60),
+              child: Container(
+                child: TextFormField(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20, right: 60, left: 60, bottom: 0),
+              child: Text(
+                'Password:',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Raleway',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 60, left: 60),
+              child: Container(
+                child: TextFormField(),
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 14, bottom: 2),
+                  child: ButtonTheme(
+                    minWidth: 200.0,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.white)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomeState()),
+                        );
+                      },
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      child: Text("Submit".toUpperCase(),
+                          style: TextStyle(fontSize: 14)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 0, right: 80, left: 80),
+                  child: ButtonTheme(
+                    minWidth: 200.0,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(22.0),
+                          side: BorderSide(color: Colors.white)),
+                      onPressed: () {
+                        startSignIn();
+                      },
+                      child: ListTile(
+                        leading:
+                            Image.asset("assets/Image/google.png", width: 30),
+                        title: Text(
+                          "Sign in with google".toUpperCase(),
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                      color: Colors.white,
+                      textColor: Colors.black,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void startSignIn() async {
+    await googleSignIn.signOut(); //optional
+    GoogleSignInAccount user = await googleSignIn.signIn();
+    if (user == null) {
+      print('Sign In Failed ');
+    } else {
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+}
 
 // class Auth {
 //   Auth({
